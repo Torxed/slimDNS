158445fd30a5d524ee73150f7908fa6fa3654719
Anton Hvornum <anton.feeds+github@gmail.com>
2020-07-19 18:25:02 +0200

    Added a epoll() replacement for windows.
    Used it in slimHTTP, seams to be working pretty transparently as a replacement for epoll() on windows platforms.

--- slimDNS/lib/servers.py
+++ slimDNS/lib/servers.py
@@ -1,8 +1,36 @@
 from socket import *
-from select import epoll, EPOLLIN
 
 from .events import *
 from .identities import *
+
+try:
+	from select import epoll, EPOLLIN
+except:
+	import select
+	EPOLLIN = None
+	class epoll():
+		""" #!if windows
+		Create a epoll() implementation that simulates the epoll() behavior.
+		This so that the rest of the code doesn't need to worry weither we're using select() or epoll().
+		"""
+		def __init__(self):
+			self.sockets = {}
+			self.monitoring = {}
+
+		def unregister(self, fileno, *args, **kwargs):
+			try:
+				del(self.monitoring[fileno])
+			except:
+				pass
+
+		def register(self, fileno, *args, **kwargs):
+			self.monitoring[fileno] = True
+
+		def poll(self, timeout=0.5, *args, **kwargs):
+			try:
+				return [[fileno, 1] for fileno in select.select(list(self.monitoring.keys()), [], [], timeout)[0]]
+			except OSError:
+				return []
 
 class TCP_SERVER():
 	def __init__(self, *args, **kwargs):
